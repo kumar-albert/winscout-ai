4a900e26b1cbc3abd0cb3988826983183c6eabaa
kumar albert <kumar.albert@ulabsystems.com>
2026-08-10 17:45:01 -0600

    makefile for windows

---
+++ make.bat
@@ -0,0 +1,39 @@
+@echo off
+setlocal
+
+if "%1"=="" goto help
+if "%1"=="help" goto help
+if "%1"=="pre-requisites" goto prerequisites
+if "%1"=="install" goto install
+if "%1"=="run" goto run
+if "%1"=="clean" goto clean
+
+echo Unknown target: %1
+goto help
+
+:help
+echo Available commands:
+echo   make.bat install         - Install dependencies
+echo   make.bat run             - Run the application
+echo   make.bat clean           - Remove __pycache__ folders
+echo   make.bat pre-requisites  - Install uv
+goto end
+
+:prerequisites
+powershell -NoProfile -Command "irm https://astral.sh/uv/install.ps1 | iex"
+goto end
+
+:install
+uv sync
+goto end
+
+:run
+uv run python main.py
+goto end
+
+:clean
+for /d /r %%d in (__pycache__) do @if exist "%%d" rd /s /q "%%d"
+goto end
+
+:end
+endlocal
